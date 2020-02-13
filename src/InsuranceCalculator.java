edafb3d308be2c8ab85170d2bb4c5f19e7f1df79
mdhimesdaily <mdhimes@bsu.edu>
2020-02-13 11:56:44 -0500

    Implementing the Abstract Class

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -8,11 +8,14 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
         } else {
             strategy = new InsuranceStrategyVeryHigh();
             return strategy.calculateInsuranceVeryHigh(income);
